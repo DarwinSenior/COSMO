d239320baf76bc46859b15c74458536274611521
darwinsenior <darwinsenior@hotmail.com>
2014-12-08 00:35:34 -0600

    changed print to py3 style

--- bincount/bincount.py
+++ bincount/bincount.py
@@ -62,6 +62,6 @@ if __name__ == '__main__':
 			count_result = concatenate((count_result, zeros(30-len(count_result), dtype=int)))
 			counter = add(counter, count_result)
 			logfile.write(str(counter))
-			print "finished grid i, j"
+			print("finished grid i, j")
 	for i in xrange(30):
 		print("bin:%d number:%d"%(i, counter[i]))
